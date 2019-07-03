153cddcd7c499f6c45661fc5e334aa9e5eb35f3e
Morten Olav Hansen <mortenoh@gmail.com>
2019-07-03 12:27:26 +0200

    pubsub example

--- typeorm-examples/src/entity/User2.ts
+++ typeorm-examples/src/entity/User2.ts
@@ -1,15 +1,38 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+  BaseEntity,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BeforeInsert,
+  AfterInsert,
+  AfterLoad,
+} from "typeorm";
 
 @Entity()
 export default class User2 extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ unique: true })
   username: string;
 
   @Column()
   isActive: boolean;
+
+  @AfterLoad()
+  afterLoad() {
+    console.log("afterLoad");
+  }
+
+  @BeforeInsert()
+  beforeInsert() {
+    console.log("beforeInsert");
+  }
+
+  @AfterInsert()
+  afterInsert() {
+    console.log("afterInsert");
+  }
 
   static findByActive(isActive: boolean) {
     return this.createQueryBuilder("user")
